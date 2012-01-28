fc970e8c96b3b5b635f7154926284d62010ff1bb
Rob Conery <rob@wekeroad.com>
2012-01-27 15:11:46 -1000

    Added thing

--- StackOverFaux.Web/config/deploy.rb
+++ StackOverFaux.Web/config/deploy.rb
@@ -16,10 +16,10 @@ role :db,  "www.stackoverfaux.com", :primary => true # This is where Rails migra
 # these http://github.com/rails/irs_process_scripts
 
 # If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "sudo /etc/init.d/monserve stop && /etc/init.d/monserve start"
+  end
+end
